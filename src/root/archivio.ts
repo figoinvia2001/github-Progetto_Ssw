dded5c38c5891c3efffbe5503831b65f845369c9
figoinvia2001 <159314231+figoinvia2001@users.noreply.github.com>
2024-06-07 18:30:52 +0200

    Update archivio.ts

--- src/root/archivio.ts
+++ src/root/archivio.ts
@@ -6,7 +6,7 @@ export class Archivio {
   }
   cerca(stringa: string) {
     var i :number = 0;
-    const one_element: Array<any> = [];
+    const trovato: Array<any> = [];
     let new_stringa = stringa.toLowerCase();
     
     if(this.libri != null){
@@ -15,11 +15,11 @@ export class Archivio {
         let new_value2 = value.titolo?.toLowerCase();
         if ((new_value1.includes(new_stringa) || new_value2.includes(new_stringa)) && new_stringa !== '') {
           i++;
-          one_element.push(value.posizione, value.autore, value.titolo, value.utente_prestito);
+          trovato.push(value.posizione, value.autore, value.titolo, value.utente_prestito);
         }
       })
     }
-    if(i==1){return one_element;
+    if(i==1){return trovato;
     }else if(i>1){return i + ' corrispondenze';
     }else{return 'Nessun risultato';
     }
